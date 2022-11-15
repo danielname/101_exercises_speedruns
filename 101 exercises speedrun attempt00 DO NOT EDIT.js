388fc1d4da6a2ab432d25239059bbd0401bccb3d
Daniel Name <daniel.v.name@gmail.com>
2022-11-15 11:08:39 -0600

    81

--- 101 exercises speedrun attempt00 DO NOT EDIT.js
+++ 101 exercises speedrun attempt00 DO NOT EDIT.js
@@ -1097,7 +1097,15 @@ addToDone("Exercise 71 is correct.");
 
 // Exercise 72
 // Write a function definition named countEvens that takes in sequence of numbers and returns the number of even numbers
-
+function countEvens(a){
+    let counter =0;
+    for (let i=0; i< a.length; i++){
+        if (isEven(a[i])){
+            counter++
+        }
+    }
+    return counter;
+}
 assert(countEvens([1, 2, 3]), 1, "Exercise 72");
 assert(countEvens([2, 5, 6]), 2, "Exercise 72");
 assert(countEvens([3, 3, 3]), 0, "Exercise 72");
@@ -1107,7 +1115,14 @@ addToDone("Exercise 72 is correct.")
 
 // Exercise 73
 // Write a function definition named hasOdds that takes in sequence of numbers and returns true if there are any odd numbers in the sequence
-
+function hasOdds(a){
+    for (let i=0; i<a.length; i++){
+        if (isOdd(a[i])){
+            return true;
+        }
+    }
+    return false;
+}
 assert(hasOdds([1, 2, 3]), true, "Exercise 73");
 assert(hasOdds([2, 5, 6]), true, "Exercise 73");
 assert(hasOdds([3, 3, 3]), true, "Exercise 73");
@@ -1117,7 +1132,15 @@ addToDone("Exercise 73 is correct.")
 
 // Exercise 74
 // Write a function definition named countOdds that takes in sequence of numbers and returns a count of the any odd numbers in the sequence
-
+function countOdds(a){
+    let counter =0;
+    for (let i=0; i< a.length; i++){
+        if (isOdd(a[i])){
+            counter++
+        }
+    }
+    return counter;
+}
 assert(countOdds([1, 2, 3]), 2, "Exercise 74");
 assert(countOdds([2, 5, 6]), 1, "Exercise 74");
 assert(countOdds([3, 3, 3]), 3, "Exercise 74");
@@ -1127,7 +1150,15 @@ addToDone("Exercise 74 is correct.")
 
 // Exercise 75
 // Write a function definition named countNegatives that takes in sequence of numbers and returns a count of the number of negative numbers
-
+function countNegatives(a){
+    let counter =0;
+    for (let i=0; i< a.length; i++){
+        if (!isPositive(a[i])){
+            counter++
+        }
+    }
+    return counter;
+}
 assert(countNegatives([1, -2, 3]), 1, "Exercise 75");
 assert(countNegatives([2, -5, -6]), 2, "Exercise 75");
 assert(countNegatives([3, 3, 3]), 0, "Exercise 75");
@@ -1136,7 +1167,15 @@ addToDone("Exercise 75 is correct.")
 
 // Exercise 76
 // Write a function definition named countPositives that takes in sequence of numbers and returns a count of the number of positive numbers
-
+function countPositives(a){
+    let counter =0;
+    for (let i=0; i< a.length; i++){
+        if (isPositive(a[i])){
+            counter++
+        }
+    }
+    return counter;
+}
 assert(countPositives([1, -2, 3]), 2, "Exercise 76");
 assert(countPositives([2, -5, -6]), 1, "Exercise 76");
 assert(countPositives([3, 3, 3]), 3, "Exercise 76");
@@ -1146,7 +1185,9 @@ addToDone("Exercise 76 is correct.")
 
 // Exercise 77
 // Write a function definition named onlyPositiveEvens that takes in sequence of numbers and returns an array containing all the positive evens from the sequence
-
+function onlyPositiveEvens(a){
+    return onlyPositiveNumbers(onlyEvenNumbers(a));
+}
 assert(onlyPositiveEvens([1, -2, 3]), [], "Exercise 77");
 assert(onlyPositiveEvens([2, -5, -6]), [2], "Exercise 77");
 assert(onlyPositiveEvens([3, 3, 4, 6]), [4, 6], "Exercise 77");
@@ -1156,7 +1197,9 @@ addToDone("Exercise 77 is correct.")
 
 // Exercise 78
 // Write a function definition named onlyPositiveOdds that takes in sequence of numbers and returns an array containing all the positive odd numbers from the sequence
-
+function onlyPositiveOdds(a){
+    return onlyPositiveNumbers(onlyOddNumbers(a));
+}
 assert(onlyPositiveOdds([1, -2, 3]), [1, 3], "Exercise 78");
 assert(onlyPositiveOdds([2, -5, -6]), [], "Exercise 78");
 assert(onlyPositiveOdds([3, 3, 4, 6]), [3, 3], "Exercise 78");
@@ -1166,7 +1209,9 @@ addToDone("Exercise 78 is correct.")
 
 // Exercise 79
 // Write a function definition named onlyNegativeEvens that takes in sequence of numbers and returns an array containing all the negative even numbers from the sequence
-
+function onlyNegativeEvens(a){
+    return onlyEvenNumbers(onlyNegativeNumbers(a));
+}
 assert(onlyNegativeEvens([1, -2, 3]), [-2], "Exercise 79");
 assert(onlyNegativeEvens([2, -5, -6]), [-6], "Exercise 79");
 assert(onlyNegativeEvens([3, 3, 4, 6]), [], "Exercise 79");
@@ -1176,7 +1221,9 @@ addToDone("Exercise 79 is correct.")
 
 // Exercise 80
 // Write a function definition named onlyNegativeOdds that takes in sequence of numbers and returns an array containing all the negative odd numbers from the sequence
-
+function onlyNegativeOdds(a){
+    return onlyOddNumbers(onlyOddNumbers(a));
+}
 assert(onlyNegativeOdds([1, -2, 3]), [], "Exercise 80");
 assert(onlyNegativeOdds([2, -5, -6]), [-5], "Exercise 80");
 assert(onlyNegativeOdds([3, 3, 4, 6]), [], "Exercise 80");
@@ -1186,7 +1233,15 @@ addToDone("Exercise 80 is correct.")
 
 // Exercise 81
 // Write a function definition named shortestString that takes in an array of strings and returns the shortest string in the array.
-
+function shortestString(a){
+    let short = "dfgbsglkjndsaklgndsajkfnlkdsnfkdsajnfklsdjngvlkfdsavbnifdsnauvnsdakvnkadsjnvksdlvnlsdnvlfrveiruvnklasfvbnklsfdajnvaskdvlksadnvjkdsfnabvlkadfsklvsdfalkvlsfvljsfklvnkdasfgjhdsaigohfsdgaoigferwubfigkvuhbfasdlkgbasdlksfvksjanvlsfjnvlsdfkaldfsgvlkfbglfdsnbgrtyoadinsujkvhfbsvdhehgdlnjblgndsajkfnlkdsnfkdsajnfklsdjngvlkfdsavbnifdsnauvnsdakvnkadsjnvksdlvnlsdnvlfrveiruvnklasfvbnklsfdajnvaskdvlksadnvjkdsfnabvlkadfsklvsdfalkvlsfvljsfklvnkdasfgjhdsaigohfsdgaoigferwubfigkvuhbfasdlkgbasdlksfvksjanvlsfjnvlsdfkaldfsgvlkfbglfdsnbgrtyoadinsujkvhfbsvdhehgdlnjbdcs;lnbfhkdijoklfdfiguheiruaphsfdrpewhgfiuhroeaifghufhiawefrioerwbfoiewrbnfjvknfsklvnfksldnvjkdsnfklgndsajkfnlkdsnfkdsajnfklsdjngvlkfdsavbnifdsnauvnsdakvnkadsjnvksdlvnlsdnvlfrveiruvnklasfvbnklsfdajnvaskdvlksadnvjkdsfnabvlkadfsklvsdfalkvlsfvljsfklvnkdasfgjhdsaigohfsdgaoigferwubfigkvuhbfasdlkgbasdlksfvksjanvlsfjnvlsdfkaldfsgvlkfbglfdsnbgrtyoadinsujkvhfbsvdhehgdlnjbdcs;lnbfhkdijoklfdfiguheiruaphsfdrpewhgfiuhroeaifghufhiawefrioerwbfoiewrbnfjvknfsklvnfksldnvjkdsnfklgndsajkfnlkdsnfkdsajnfklsdjngvlkfdsavbnifdsnauvnsdakvnkadsjnvksdlvnlsdnvlfrveiruvnklasfvbnklsfdajnvaskdvlksadnvjkdsfnabvlkadfsklvsdfalkvlsfvljsfklvnkdasfgjhdsaigohfsdgaoigferwubfigkvuhbfasdlkgbasdlksfvksjanvlsfjnvlsdfkaldfsgvlkfbglfdsnbgrtyoadinsujkvhfbsvdhehgdlnjbdcs;lnbfhkdijoklfdfiguheiruaphsfdrpewhgfiuhroeaifghufhiawefrioerwbfoiewrbnfjvknfsklvnfksldnvjkdsnfklgndsajkfnlkdsnfkdsajnfklsdjngvlkfdsavbnifdsnauvnsdakvnkadsjnvksdlvnlsdnvlfrveiruvnklasfvbnklsfdajnvaskdvlksadnvjkdsfnabvlkadfsklvsdfalkvlsfvljsfklvnkdasfgjhdsaigohfsdgaoigferwubfigkvuhbfasdlkgbasdlksfvksjanvlsfjnvlsdfkaldfsgvlkfbglfdsnbgrtyoadinsujkvhfbsvdhehgdlnjbdcs;lnbfhkdijoklfdfiguheiruaphsfdrpewhgfiuhroeaifghufhiawefrioerwbfoiewrbnfjvknfsklvnfksldnvjkdsnfklgndsajkfnlkdsnfkdsajnfklsdjngvlkfdsavbnifdsnauvnsdakvnkadsjnvksdlvnlsdnvlfrveiruvnklasfvbnklsfdajnvaskdvlksadnvjkdsfnabvlkadfsklvsdfalkvlsfvljsfklvnkdasfgjhdsaigohfsdgaoigferwubfigkvuhbfasdlkgbasdlksfvksjanvlsfjnvlsdfkaldfsgvlkfbglfdsnbgrtyoadinsujkvhfbsvdhehgdlnjbdcs;lnbfhkdijoklfdfiguheiruaphsfdrpewhgfiuhroeaifghufhiawefrioerwbfoiewrbnfjvknfsklvnfksldnvjkdsnfklgndsajkfnlkdsnfkdsajnfklsdjngvlkfdsavbnifdsnauvnsdakvnkadsjnvksdlvnlsdnvlfrveiruvnklasfvbnklsfdajnvaskdvlksadnvjkdsfnabvlkadfsklvsdfalkvlsfvljsfklvnkdasfgjhdsaigohfsdgaoigferwubfigkvuhbfasdlkgbasdlksfvksjanvlsfjnvlsdfkaldfsgvlkfbglfdsnbgrtyoadinsujkvhfbsvdhehgdlnjbdcs;lnbfhkdijoklfdfiguheiruaphsfdrpewhgfiuhroeaifghufhiawefrioerwbfoiewrbnfjvknfsklvnfksldnvjkdsnfklgndsajkfnlkdsnfkdsajnfklsdjngvlkfdsavbnifdsnauvnsdakvnkadsjnvksdlvnlsdnvlfrveiruvnklasfvbnklsfdajnvaskdvlksadnvjkdsfnabvlkadfsklvsdfalkvlsfvljsfklvnkdasfgjhdsaigohfsdgaoigferwubfigkvuhbfasdlkgbasdlksfvksjanvlsfjnvlsdfkaldfsgvlkfbglfdsnbgrtyoadinsujkvhfbsvdhehgdlnjbdcs;lnbfhkdijoklfdfiguheiruaphsfdrpewhgfiuhroeaifghufhiawefrioerwbfoiewrbnfjvknfsklvnfksldnvjkdsnfklgndsajkfnlkdsnfkdsajnfklsdjngvlkfdsavbnifdsnauvnsdakvnkadsjnvksdlvnlsdnvlfrveiruvnklasfvbnklsfdajnvaskdvlksadnvjkdsfnabvlkadfsklvsdfalkvlsfvljsfklvnkdasfgjhdsaigohfsdgaoigferwubfigkvuhbfasdlkgbasdlksfvksjanvlsfjnvlsdfkaldfsgvlkfbglfdsnbgrtyoadinsujkvhfbsvdhehgdlnjbdcs;lnbfhkdijoklfdfiguheiruaphsfdrpewhgfiuhroeaifghufhiawefrioerwbfoiewrbnfjvknfsklvnfksldnvjkdsnfkdcs;lnbfhkdijoklfdfiguheiruaphsfdrpewhgfiuhroeaifghufhiawefrioerwbfoiewrbnfjvknfsklvnfksldnvjkdsnfkndskfnkdsanfvs";
+    for (let i=0; i< a.length; i++){
+        if (a[i].length < short.length){
+            short = a[i];
+        }
+    }
+    return short;
+}
 assert(shortestString(["kiwi", "mango", "strawberry"]), "kiwi", "Exercise 81");
 assert(shortestString(["hello", "everybody"]), "hello", "Exercise 81");
 assert(shortestString(["mary", "had", "a", "little", "lamb"]), "a", "Exercise 81");
